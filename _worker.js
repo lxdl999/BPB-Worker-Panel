07455adf26c0854021ebbf5ed23705e503b068e6
lxdl999 <166361123+lxdl999@users.noreply.github.com>
2024-09-16 15:07:23 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = 'b26ceb06-0a1e-4dd6-8042-cc5a19a49d01';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
